80ff5d907cf193d17071a5a98e014cfa96ca6f4e
Jenil Savalia <jenil.savalia.cd@gmail.com>
2024-12-04 14:47:23 +0530

    hell

---
+++ src/landing.jsx
@@ -0,0 +1,84 @@
+import './App.css';
+
+function Landing() {
+    return (
+        <>
+            <div id="landing">
+                <div className="landing_banner">
+                    <div className="landing_header">
+                        Learn to craft better <span id="text_corosol">Stories</span>
+                    </div>
+                    <div className="landing_para">
+                        The best video editing & storytelling cohort that money can buy.
+                    </div>
+                    <div className="landing_ratings">
+                        <img
+                            className="rating_arrow"
+                            src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/660cef178e07a85971dbe2c8_Vector.svg"
+                            alt=""
+                        />
+                        <div className="primary_btn">
+                            <span>Join now</span>
+                            <img
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/662b19fd4bcac95951b2d27b_Vector.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className="ratings_mid_line"></div>
+                        <div className="five_star_rate">
+                            <img
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66f7cbf06a76b8022b0d7cc4_Stars.png"
+                                alt=""
+                            />
+                            <p>4.8 Rating</p>
+                        </div>
+                    </div>
+
+                    <div class="get_hired_by">
+                        <p>Get Hired By</p>
+                        <div class="slide_container">
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66f7cd23cda48ae10d1e0b44_channels4_profile%20(1).jpg"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd1f8c5e796eb071b5ba7d_download.png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd1f4b31d12787c44c3b92_images.png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd1fd93a623477ad9f7cc1_download%20(1).png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66f7cd23bb0f45ece8b362b4_channels4_profile%20(2).jpg"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd8a2fff10712218ef5438_Brand%20Flow%20Final-14-p-500.png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd2036806784a9031abb16_zfvkuf2k98lacoqpqdiw.webp"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd209d18c4db12f4189dbc_images%20(1).png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd20ed5e796eb071b71fa0_images%20(2).png"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66fd21334486399c53f45bce_images%20(1).jpeg"
+                                alt=""></img>
+                            <img class="get_hired_by_img"
+                                src="https://cdn.prod.website-files.com/660b8797bc7ef434f6a3c823/66f7cd23cda48ae10d1e0b44_channels4_profile%20(1).jpg"
+                                alt="">
+                            </img>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Landing;
